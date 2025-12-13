b1571131ea006b8a2d5d40c5be3a394348aedec4
ulubahsigokhan <g.ulubahsi.tech@gmail.com>
2025-12-12 20:23:56 -0800

    Add win and draw checking methods to Board

--- src/tictactoe/Board.java
+++ src/tictactoe/Board.java
@@ -31,7 +31,7 @@ public class Board {
      */
     public void printBoard() {
 
-        String horizontal = "-".repeat(SIZE + (SIZE -1) * 3);
+        String horizontal = "-".repeat(SIZE + (SIZE - 1) * 3);
 
         for (int i = 0; i < SIZE; i++) {
 
@@ -55,6 +55,94 @@ public class Board {
         }
     }
 
-}
+    /**
+     * This methods checks if row and col are within the bounds and
+     * if the chosen cell is empty. It places the symbol and if true indicates success.
+     *
+     * @param row
+     * @param col
+     * @param symbol
+     * @return
+     */
+    public boolean placeMove(int row, int col, char symbol) {
+        // Checks if row is within bounds
+        if (row < 0 || row >= SIZE) {
+            return false;
+        }
+        // Checks if col is within bounds
+        if (col < 0 || col >= SIZE) {
+            return false;
+        }
+        // Checks if the choosen cell is empty
+        if (board[row][col] != ' ') {
+            return false;
+        }
+        // Placing the symbol
+        board[row][col] = symbol;
 
+        // Indicate success
+        return true;
+    }
+
+    /**
+     * This method goes true the rows and columns and also diagonal/anti-diagonal
+     * to check which player is the winner
+     * @return
+     */
+    public boolean checkWinner() {
+        // Rows check
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i][0] != ' ' &&
+                    board[i][0] == board[i][1] &&
+                    board[i][1] == board[i][2]) {
+                return true;
+            }
+        }
+        // Cols check
+        for (int i = 0; i < SIZE; i++) {
+            if (board[0][i] != ' ' &&
+                    board[0][i] == board[1][i] &&
+                    board[1][i] == board[2][i]) {
+                return true;
+            }
+        }
+        // Diagonal check
+        if (board[0][0] != ' ' &&
+                board[0][0] == board[1][1] &&
+                board[1][1] == board[2][2]) {
+            return true;
+        }
+        // Anti-diagonal check
+        if (board[0][2] != ' ' &&
+                board[0][2] == board[1][1] &&
+                board[1][1] == board[2][0]) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if the game is a draw
+     * A draw means the board is full and no player has won
+     * @return
+     */
+    public boolean isDraw() {
+        // If there is a winner, it cannot be a draw
+        if (checkWinner()) {
+            return false;
+        }
+        // Check if any empty space remains
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        // No winner and no empty spaces -> draw
+        return true;
+    }
+
+}
 
